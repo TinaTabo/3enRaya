eaa71fbd7b3191f3d3f68d9196833e7df8ba8878
Cristina Taboada <cris.taboada98@gmail.com>
2023-06-06 11:58:05 +0200

    componente cuadricula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CuadriculaComponent } from './component/cuadricula/cuadricula.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CuadriculaComponent
   ],
   imports: [
     BrowserModule,
