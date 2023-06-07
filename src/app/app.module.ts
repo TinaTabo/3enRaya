1312d73843d5437e699ad06f83985e877552b101
Cristina Taboada <cris.taboada98@gmail.com>
2023-06-07 15:17:23 +0200

    añadido componente tablero + estilos generales + favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableroComponent } from './component/tablero/tablero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableroComponent
   ],
   imports: [
     BrowserModule,
